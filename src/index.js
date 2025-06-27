0d842da9ceaa70dfa5bf3ab9a35628befc7b3ad5
Jobrzut <julek@geminidesign.pl>
2025-06-27 20:24:09 +0200

    Add dynamically html structure for menu page

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { homepage } from "./home.js";
+import { menu } from "./menu.js";
 import "./css/global.css";
 import "./css/nav.css";
 import "./css/homepage.css";
@@ -7,14 +8,23 @@ import "./css/footer.css";
 homepage();
 
 const nav = document.querySelector("nav");
+const navButtons = document.querySelectorAll("nav button");
 const contentDiv = document.querySelector("#content");
+
+function cleanDiv() {
+  contentDiv.innerHTML = "";
+  navButtons.forEach(element => {
+    element.classList.remove("active");
+  });
+}
 
 nav.addEventListener("click", (event) => {
   if (event.target.textContent === "Home") {
-    contentDiv.innerHTML = "";
+    cleanDiv();
     homepage();
   } else if (event.target.textContent === "Menu") {
-    
+    cleanDiv();
+    menu();
   } else if (event.target.textContent === "About") {
     
   } else if (event.target.textContent === "Contact") {
---
+++ src/menu.js
@@ -0,0 +1,47 @@
+const contentDiv = document.querySelector("#content");
+const navButtons = document.querySelectorAll("nav button");
+
+const menuItems = {
+  chicken: {
+    emoji: "🍗",
+    price: 16,
+  },
+  pizza: {
+    emoji: "🍕",
+    price: 22,
+  },
+  taco: {
+    emoji: "🌮",
+    price: 18,
+  },
+  frenchFries: {
+    emoji: "🍟",
+    price: 12,
+  },
+  hamburger: {
+    emoji: "🍔",
+    price: 20,
+  },
+  hotdog: {
+    emoji: "🌭",
+    price: 14,
+  },
+};
+
+export function menu() {
+  const gridDiv = document.createElement("div");
+  Object.entries(menuItems).forEach(([title, element]) => {
+    const card = document.createElement("div");
+    const h2Emoji = document.createElement("h2");
+    const h2Title = document.createElement("h2");
+    const pPrice = document.createElement("p");
+
+    h2Emoji.textContent = element.emoji;
+    h2Title.textContent = title;
+    pPrice.textContent = "$ " + element.price;
+    card.append(h2Emoji, h2Title, pPrice);
+    gridDiv.appendChild(card);
+  });
+  contentDiv.appendChild(gridDiv);
+  navButtons[1].className = "active";
+}
